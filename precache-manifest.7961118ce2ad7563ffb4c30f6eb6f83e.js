a7f28afde42b018d9a2bab96842b48a7565f96e3
andrewmolten <andrewmolten23@gmail.com>
2023-02-12 14:23:44 +1300

    Updates

---
+++ precache-manifest.7961118ce2ad7563ffb4c30f6eb6f83e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8167ff4d3f7923ec24d8",
+    "url": "/robofriends/static/css/main.52a6bc9a.chunk.css"
+  },
+  {
+    "revision": "8167ff4d3f7923ec24d8",
+    "url": "/robofriends/static/js/main.8167ff4d.chunk.js"
+  },
+  {
+    "revision": "ee2fa2fc286181834050",
+    "url": "/robofriends/static/js/1.ee2fa2fc.chunk.js"
+  },
+  {
+    "revision": "47dc804321408d7d7422",
+    "url": "/robofriends/static/css/2.977e4c6c.chunk.css"
+  },
+  {
+    "revision": "47dc804321408d7d7422",
+    "url": "/robofriends/static/js/2.47dc8043.chunk.js"
+  },
+  {
+    "revision": "0e09ba9c7502e90c21a0",
+    "url": "/robofriends/static/js/runtime~main.0e09ba9c.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/robofriends/static/media/SEGA.82e09f78.TTF"
+  },
+  {
+    "revision": "44f691bb477027a57ecaf2ac42b3b6fd",
+    "url": "/robofriends/index.html"
+  }
+];
